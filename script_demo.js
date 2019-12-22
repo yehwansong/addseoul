cd63f147e0ec83749a66bddbec1b7bd0e4de73ca
yehwansong <yehwanyensong@gmail.com>
2019-12-23 02:41:12 +0900

    add

---
+++ script_demo.js
@@ -0,0 +1,5 @@
+
+    $('.cat_BOTTOM').append('<ul class="menuCategory bottom_sub">
+         <li style="display:;" class="xans-element- xans-product xans-product-displaycategory  xans-record-"><a href="/category/slacks/117/">SLACKS <span class="count displaynone">()</span></a></li>
+		<li style="display:;" class="xans-element- xans-product xans-product-displaycategory  xans-record-"><a href="/category/denim/116/">DENIM <span class="count displaynone">()</span></a></li>
+	</ul>')
